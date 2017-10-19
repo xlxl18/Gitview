8966423cfd33ecf5087a9ac7d40af95937ab2d40
Timur Yegemberdiev <tima_93@list.ru>
2017-10-19 14:34:43 +0600

    Added new class

---
+++ src/main/java/cai/local/gitview/BaseEntity.java
@@ -0,0 +1,18 @@
+package cai.local.gitview;
+
+public class BaseEntity {
+    public Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
